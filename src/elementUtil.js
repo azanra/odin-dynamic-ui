d312a269d8d1d0b1675df2628f4e6a16ca448ae6
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-04 09:20:48 +0700

    Add method to append text to the new element

--- src/elementUtil.js
+++ src/elementUtil.js
@@ -1,15 +1,23 @@
 const elementUtil = (function () {
   const createElement = (attribute) => {
-    const { elementType, elementAttribute } = attribute;
+    const { elementType, elementAttribute, textContent } = attribute;
     const element = document.createElement(elementType);
     setMultipleAttributeToElement(element, elementAttribute);
+    appendTextToElement(element, textContent);
+    return element;
   };
 
-  const setMultipleAttributeToElement = (element, attribute) => {
-    for (const attr in attribute) {
-      element.setAttribute(attr, attribute[attr]);
+  const setMultipleAttributeToElement = (element, elementAttribute) => {
+    for (const attribute in elementAttribute) {
+      element.setAttribute(attr, elementAttribute[attribute]);
     }
   };
+
+  const appendTextToElement = (element, text) => {
+    element.textContent = text;
+  };
+
+  return { createElement, setMultipleAttributeToElement, appendTextToElement };
 })();
 
 export default elementUtil;
