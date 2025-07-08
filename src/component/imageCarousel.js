2c8eb60122b0c1c24c4902e07c13048628046fca
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-08 08:36:50 +0700

    Add method to update the img index state

--- src/component/imageCarousel.js
+++ src/component/imageCarousel.js
@@ -15,6 +15,26 @@ const imgAttr = {
 };
 
 const imageCarousel = (imgSrc) => {
+  const state = {
+    imgIndex: 0,
+  };
+
+  const incrementImgIndex = () => {
+    if (imgIndex < imgSrc.length) {
+      state.imgIndex = state.imgIndex++;
+    } else {
+      state.imgIndex = 0;
+    }
+  };
+
+  const decrementImgIndex = () => {
+    if (imgIndex > 0) {
+      state.imgIndex = state.imgIndex--;
+    } else {
+      state.imgIndex = imgSrc.length - 1;
+    }
+  };
+
   const renderCarousel = () => {
     const imageCarousel = elementUtil.createElement(imageCarouselAttr);
     imageCarousel.appendChild(renderImg());
